1977672925aac2264515e17ba2bb376a601b6539
Arvind <arvindsudarshan18@gmail.com>
2025-01-08 22:00:57 -0800

    Queries to test the model

---
+++ test.py
@@ -0,0 +1,6 @@
+from model import recipe_assistant
+
+# print("What can I cook with chicken and tomato?", "\n", recipe_assistant("What can I cook with chicken and tomato?"))
+# print("What do I do with leftover rice?", "\n", recipe_assistant("What do I do with leftover rice?"))
+# print("Quick Indian food recipes?", "\n", recipe_assistant("Quick Indian food recipes?"))
+print("what can I do with green onions, bell peppers, sweet onions and rice?", "\n", recipe_assistant("what can I do with green onions, bell peppers, sweet onions and rice?"))
